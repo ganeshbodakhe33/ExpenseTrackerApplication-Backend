3889214f38a1cae05f124c350fe459e9b286d70c
groelse <groelsse@gmail.com>
2026-02-10 10:19:49 +0530

    Updated expense request again

--- src/main/java/com/gb/ExpenseTrackerApplication/dto/ExpenseRequest.java
+++ src/main/java/com/gb/ExpenseTrackerApplication/dto/ExpenseRequest.java
@@ -1,18 +1,14 @@
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
+package com.gb.ExpenseTrackerApplication.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class ExpenseRequest {
 
-    @NotBlank
     private String title;
-
-    @Positive
     private Double amount;
-
-    @NotBlank
-    private String category;
-
-    @NotNull
     private LocalDate date;
+    private Long categoryId;
 }
